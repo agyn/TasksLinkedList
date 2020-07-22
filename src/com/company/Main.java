d09c5f247b5bb701b7854dc9d1c0279d4eb7bedc
agyn <agyn_1992@mail.ru>
2020-07-22 15:17:01 +0600

    list tasks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import com.company.tasks.DeleteMiddleNode;
 import com.company.tasks.LinkedListPartion;
 import com.company.tasks.Palindrom.IterativePalindrome;
+import com.company.tasks.Palindrom.RecursivePalindrome;
 import com.company.tasks.Palindrom.SimplePalindrome;
 import com.company.tasks.RemoveDuplicates;
 import com.company.tasks.SumLists;
@@ -81,16 +82,100 @@ public class Main {
 //        MySingleLinkedList<Integer> list3 = new MySingleLinkedList<>();
 //        var aa = SumLists.SumLists(list.head, list2.head, 0, true, list3);
 
-        MySingleLinkedList<Integer> list = new MySingleLinkedList<>();
-        list.append(0);
-        list.append(1);
-        list.append(2);
-        list.append(1);
-        list.append(0);
+//        MySingleLinkedList<Integer> list = new MySingleLinkedList<>();
+//        list.append(0);
+//        list.append(1);
+//        list.append(2);
+//        list.append(1);
+//        list.append(0);
 
 //        boolean result = SimplePalindrome.isPalindrome(list.head);
-        boolean result = IterativePalindrome.isPalindrome(list.head);
-        System.out.print(result);
+//        boolean result = IterativePalindrome.isPalindrome(list.head);
+//        boolean result = RecursivePalindrome.isPalindrome(list.head);
+//        System.out.print(result);
+
+//        ListNode n1 = new ListNode(1);
+//        ListNode n2 = new ListNode(2);
+//        n1.next = n2;
+//        ListNode n3 = new ListNode(4);
+//        n2.next = n3;
+//
+//
+//        ListNode n4 = new ListNode(1);
+//        ListNode n5 = new ListNode(3);
+//        n4.next = n5;
+//        ListNode n6 = new ListNode(4);
+//        n5.next = n6;
+//
+//        mergeTwoLists(n1, n4);
+
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(1);
+        n1.next = n2;
+        ListNode n3 = new ListNode(2);
+        n2.next = n3;
+//        ListNode n4 = new ListNode(3);
+//        n3.next = n4;
+//        ListNode n5 = new ListNode(3);
+//        n4.next = n5;
+
+        var aa = removeDuplicates(n1);
+
     }
 
+    public static ListNode removeDuplicates(ListNode head) {
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode inner = current;
+
+            while (inner != null && inner.next != null) {
+
+                if (current.val == inner.next.val) {
+                    inner.next = inner.next.next;
+                }
+                inner = inner.next;
+            }
+
+            current = current.next;
+        }
+
+        return head;
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        if (l1 == null && l2 == null) {
+            return null;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1 == null) {
+            return l2;
+        }
+
+
+        ListNode l3 = new ListNode();
+        var start = l3;
+
+        while (l1 != null || l2 != null) {
+
+            if (l1 != null && (l2 == null || l1.val <= l2.val)) {
+                l3.val = l1.val;
+                l1 = l1.next;
+            } else if (l2 != null || (l1 == null || l2.val < l1.val)) {
+                l3.val = l2.val;
+                l2 = l2.next;
+            }
+
+            if (l1 != null || l2 != null) {
+                l3.next = new ListNode();
+                l3 = l3.next;
+            }
+        }
+
+        return start;
+    }
+
+
 }
+
--- src/com/company/tasks/Palindrom/RecursivePalindrome.java
+++ src/com/company/tasks/Palindrom/RecursivePalindrome.java
@@ -1,4 +1,51 @@
 package com.company.tasks.Palindrom;
 
+import com.company.SingleNode;
+
 public class RecursivePalindrome {
+    public static boolean isPalindrome(SingleNode<Integer> head) {
+        int length = getLength(head);
+        return recursePalindrome(head, length)._result;
+    }
+
+    private static Result recursePalindrome(SingleNode<Integer> node, int length) {
+        if (node == null || length <= 0) {
+            return new Result(node, true);
+        } else if (length == 1) {
+            return new Result(node.next, true);
+        }
+
+        Result res = recursePalindrome(node.next, length -2);
+
+        if(!res._result || res._node == null){
+            return res;
+        }
+
+        res._result = (node._value == res._node._value);
+
+        res._node = res._node.next;
+
+        return res;
+    }
+
+    private static int getLength(SingleNode<Integer> node) {
+        int length = 0;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+
+        return length;
+    }
+
+}
+
+class Result {
+    public SingleNode<Integer> _node;
+    public boolean _result;
+
+    public Result(SingleNode<Integer> node, boolean result) {
+        _node = node;
+        _result = result;
+    }
 }
